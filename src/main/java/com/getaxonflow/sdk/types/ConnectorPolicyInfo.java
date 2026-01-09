62805a583a8a8cadfe94ef9a67145438bd81bae3
Saurabh Jain <saurabhjain1592@gmail.com>
2026-01-09 01:39:00 +0100

    feat: add PolicyInfo fields to ConnectorResponse
    Add new fields for MCP policy enforcement visibility:
    - isRedacted(): boolean flag for redacted responses
    - getRedactedFields(): list of JSON paths that were redacted
    - getPolicyInfo(): ConnectorPolicyInfo with evaluation metadata
    - PolicyMatchInfo: class for matched policy details
    
    Supports axonflow-enterprise #963, #975

---
+++ src/main/java/com/getaxonflow/sdk/types/ConnectorPolicyInfo.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2025 AxonFlow
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.getaxonflow.sdk.types;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Policy evaluation information included in MCP responses.
+ *
+ * Provides transparency into policy enforcement decisions for
+ * request blocking and response redaction.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public final class ConnectorPolicyInfo {
+
+    @JsonProperty("policies_evaluated")
+    private final int policiesEvaluated;
+
+    @JsonProperty("blocked")
+    private final boolean blocked;
+
+    @JsonProperty("block_reason")
+    private final String blockReason;
+
+    @JsonProperty("redactions_applied")
+    private final int redactionsApplied;
+
+    @JsonProperty("processing_time_ms")
+    private final long processingTimeMs;
+
+    @JsonProperty("matched_policies")
+    private final List<PolicyMatchInfo> matchedPolicies;
+
+    public ConnectorPolicyInfo(
+            @JsonProperty("policies_evaluated") int policiesEvaluated,
+            @JsonProperty("blocked") boolean blocked,
+            @JsonProperty("block_reason") String blockReason,
+            @JsonProperty("redactions_applied") int redactionsApplied,
+            @JsonProperty("processing_time_ms") long processingTimeMs,
+            @JsonProperty("matched_policies") List<PolicyMatchInfo> matchedPolicies) {
+        this.policiesEvaluated = policiesEvaluated;
+        this.blocked = blocked;
+        this.blockReason = blockReason;
+        this.redactionsApplied = redactionsApplied;
+        this.processingTimeMs = processingTimeMs;
+        this.matchedPolicies = matchedPolicies != null ? matchedPolicies : Collections.emptyList();
+    }
+
+    public int getPoliciesEvaluated() {
+        return policiesEvaluated;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public String getBlockReason() {
+        return blockReason;
+    }
+
+    public int getRedactionsApplied() {
+        return redactionsApplied;
+    }
+
+    public long getProcessingTimeMs() {
+        return processingTimeMs;
+    }
+
+    public List<PolicyMatchInfo> getMatchedPolicies() {
+        return matchedPolicies;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConnectorPolicyInfo that = (ConnectorPolicyInfo) o;
+        return policiesEvaluated == that.policiesEvaluated &&
+               blocked == that.blocked &&
+               redactionsApplied == that.redactionsApplied &&
+               processingTimeMs == that.processingTimeMs &&
+               Objects.equals(blockReason, that.blockReason) &&
+               Objects.equals(matchedPolicies, that.matchedPolicies);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(policiesEvaluated, blocked, blockReason, redactionsApplied, processingTimeMs, matchedPolicies);
+    }
+
+    @Override
+    public String toString() {
+        return "ConnectorPolicyInfo{" +
+               "policiesEvaluated=" + policiesEvaluated +
+               ", blocked=" + blocked +
+               ", blockReason='" + blockReason + '\'' +
+               ", redactionsApplied=" + redactionsApplied +
+               ", processingTimeMs=" + processingTimeMs +
+               ", matchedPolicies=" + matchedPolicies +
+               '}';
+    }
+}
--- src/main/java/com/getaxonflow/sdk/types/ConnectorResponse.java
+++ src/main/java/com/getaxonflow/sdk/types/ConnectorResponse.java
@@ -18,6 +18,8 @@ package com.getaxonflow.sdk.types;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -44,19 +46,34 @@ public final class ConnectorResponse {
     @JsonProperty("processing_time")
     private final String processingTime;
 
+    @JsonProperty("redacted")
+    private final boolean redacted;
+
+    @JsonProperty("redacted_fields")
+    private final List<String> redactedFields;
+
+    @JsonProperty("policy_info")
+    private final ConnectorPolicyInfo policyInfo;
+
     public ConnectorResponse(
             @JsonProperty("success") boolean success,
             @JsonProperty("data") Object data,
             @JsonProperty("error") String error,
             @JsonProperty("connector_id") String connectorId,
             @JsonProperty("operation") String operation,
-            @JsonProperty("processing_time") String processingTime) {
+            @JsonProperty("processing_time") String processingTime,
+            @JsonProperty("redacted") boolean redacted,
+            @JsonProperty("redacted_fields") List<String> redactedFields,
+            @JsonProperty("policy_info") ConnectorPolicyInfo policyInfo) {
         this.success = success;
         this.data = data;
         this.error = error;
         this.connectorId = connectorId;
         this.operation = operation;
         this.processingTime = processingTime;
+        this.redacted = redacted;
+        this.redactedFields = redactedFields != null ? redactedFields : Collections.emptyList();
+        this.policyInfo = policyInfo;
     }
 
     public boolean isSuccess() {
@@ -83,21 +100,36 @@ public final class ConnectorResponse {
         return processingTime;
     }
 
+    public boolean isRedacted() {
+        return redacted;
+    }
+
+    public List<String> getRedactedFields() {
+        return redactedFields;
+    }
+
+    public ConnectorPolicyInfo getPolicyInfo() {
+        return policyInfo;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ConnectorResponse that = (ConnectorResponse) o;
         return success == that.success &&
+               redacted == that.redacted &&
                Objects.equals(data, that.data) &&
                Objects.equals(error, that.error) &&
                Objects.equals(connectorId, that.connectorId) &&
-               Objects.equals(operation, that.operation);
+               Objects.equals(operation, that.operation) &&
+               Objects.equals(redactedFields, that.redactedFields) &&
+               Objects.equals(policyInfo, that.policyInfo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(success, data, error, connectorId, operation);
+        return Objects.hash(success, data, error, connectorId, operation, redacted, redactedFields, policyInfo);
     }
 
     @Override
@@ -106,6 +138,7 @@ public final class ConnectorResponse {
                "success=" + success +
                ", connectorId='" + connectorId + '\'' +
                ", operation='" + operation + '\'' +
+               ", redacted=" + redacted +
                ", error='" + error + '\'' +
                '}';
     }
